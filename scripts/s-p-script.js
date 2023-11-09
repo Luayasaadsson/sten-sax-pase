422b5c16125f0df807b10cdcb1808e18641e74aa
simon.lundmark1 <simon.lundmark@chasacademy.se>
2023-11-09 15:09:51 +0100

    express js, data > json + redirect till index.html

--- scripts/s-p-script.js
+++ scripts/s-p-script.js
@@ -1,27 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   let selectedCharacter = null;
 
-  // Hantera klick på karaktärsbilder
+  // Handle clicks on character images
   document.querySelectorAll('.img').forEach(img => {
-      img.addEventListener('click', function() {
-          if (selectedCharacter) {
-              selectedCharacter.classList.remove('selected');
-          }
-          this.classList.add('selected');
-          selectedCharacter = this;
-      });
+    img.addEventListener('click', function() {
+      if (selectedCharacter) {
+        selectedCharacter.classList.remove('selected');
+      }
+      this.classList.add('selected');
+      selectedCharacter = this;
+    });
   });
 
-  // Spara val och namn när användaren klickar på 'Continue'
+  // Save choice and name when user clicks 'Continue'
   document.getElementById('continueButton').addEventListener('click', () => {
-      const userName = document.getElementById('userNameInput').value;
-      const robotName = document.getElementById('robotNameInput').value;
-      const characterImage = selectedCharacter ? selectedCharacter.src : '';
+    const userName = document.getElementById('userNameInput').value;
+    const robotName = document.getElementById('robotNameInput').value;
+    const characterImage = selectedCharacter ? selectedCharacter.src : '';
 
-      localStorage.setItem('userName', userName);
-      localStorage.setItem('robotName', robotName);
-      localStorage.setItem('characterImage', characterImage);
+    // Create a data object with user input
+    const data = {
+      userName: userName,
+      robotName: robotName,
+      characterImage: characterImage
+    };
 
-      // Här kan du lägga till kod för att navigera till nästa sida
-  });
-});
+    // Send the data object to the server
+    
+    fetch('http://localhost:3000/data.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+      // Handle response data
+      console.log(responseData);
+      // Redirect to index.html after successful POST request
+    
+      window.location.href = 'index.html';
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error:', error);
+
+    });
+    window.location.href = 'index.html';
+  }); // Close of the 'continueButton' click event listener
+}); // Close of the 'DOMContentLoaded' event listener
