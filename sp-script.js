b35baf54a693882b13f1c7aa265d48dc53ad5d3d
robinruden <robinruden@gmail.com>
2023-11-14 17:37:06 +0100

    big fail med avatarers

--- sp-script.js
+++ sp-script.js
@@ -1,16 +1,18 @@
-function sparaFormular(){
+// Sparar ner namnen till huvudsidan
+function sparaFormular(){ // Funktionen triggas av knappen Continue från secong page
     let humanName = document.getElementById('userNameInput').value;
     let robotName = document.getElementById('robotNameInput').value;
 
 localStorage.setItem('mittHumanName', humanName);
 localStorage.setItem('mittRobotName', robotName);
 
-uppdatteraVisning();
+uppdatteraVisning(); //Kör i gång funktionen nedan
 
 return false;
 
 }
 
+// Laddar upp namnen på huvudsidan
 function uppdatteraVisning(){
     let sparatHumanName = localStorage.getItem('mittHumanName');
     let sparatRobotName = localStorage.getItem('mittRobotName');
@@ -25,5 +27,55 @@ function uppdatteraVisning(){
     }
 };
 
-window.onload = uppdatteraVisning;
 
+
+
+/* window.onload = uppdatteraVisning; */
+
+
+
+/* function sparaAvatar() {
+    document.querySelectorAll('.img').forEach(function(bild) {
+        bild.addEventListener('click', function() {
+            let avatar = this.src;
+            localStorage.setItem('sparadAvatar', avatar);
+        });
+    });
+} */
+
+
+const avatarIds = ['demon', 'man', 'alien', 'ape'];
+
+function sparaAvatar() {
+    avatarIds.forEach(function(id) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener('click', function() {
+                console.log("Avatar klickad: " + this.src); // För felsökning
+                localStorage.setItem('sparadAvatar', this.src);
+            });
+        } else {
+            console.log("Elementet hittades inte: " + id); // För felsökning
+        }
+    });
+}
+
+
+// Visa sparad avatar
+function visaSparadAvatar() {
+    let sparadUrl = localStorage.getItem('sparadAvatar');
+    if (sparadUrl) {
+        let avatarBild = document.getElementById('avatarBild');
+        if (avatarBild) {
+            avatarBild.src = sparadUrl;
+        }
+    }
+}
+
+
+
+window.onload = function() {
+    uppdatteraVisning();
+    sparaAvatar();
+    visaSparadAvatar();
+};
